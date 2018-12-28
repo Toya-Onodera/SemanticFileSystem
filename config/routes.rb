e85734dbb13622e3321bd6e0e2417e89cdfb766c
Toya-Onodera <gtrzexel@gmail.com>
2018-12-28 14:09:55 +0900

    View と API 両方のルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,23 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :files
+
+  ### Files View ###
+  namespace :files do
+    get     '/files'         =>    'files#index'
+    get     '/files/new'     =>    'files#new'
+    post    '/files'         =>    'files#create'
+    get     '/files/:id'   =>    'files#show'
+    delete  '/files/:id'     =>    'files#destroy'
+  end
+
+  ### API ###
+  namespace :api do
+    namespace :v1 do
+      get     '/sfs'         =>    'sfs#index'
+      post    '/sfs'         =>    'sfs#create'
+      get     '/sfs/:id'   =>    'sfs#show'
+      delete  '/sfs/:id'     =>    'sfs#destroy'
+    end
+  end
 end
