e646a6dd322fad2dbb6a24804baf7061346a859d
Toya-Onodera <gtrzexel@gmail.com>
2018-12-29 15:47:41 +0900

    暫定的なコントローラーをsakusei

---
+++ app/controllers/api/v1/sfs_controller.rb
@@ -0,0 +1,31 @@
+module Api
+  module V1
+
+    VERSION = '0.0.1'
+
+    class FilesController < ApplicationController
+
+      # ファイル一覧 API
+      # システムにアップロードされている条件に一致するファイル情報のデータを返す
+      # パラメータによって LIKE 検索も可能
+      def index
+      end
+
+      # ファイルアップロード API
+      # POST で送られてきたファイルを DB に格納する
+      def create
+      end
+
+      # ファイル詳細 API
+      # パラメータに数字を指定した場合に file.id に合致するものを返す
+      def show
+      end
+
+      # ファイル削除 API
+      # 指定した ID のファイルを削除する
+      def destroy
+      end
+
+    end
+  end
+end
